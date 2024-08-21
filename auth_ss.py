c7bb772d2b7086eb610ae4fbc0b5241a0137c341
Yago <yago.ribeiro.bruno@hotmail.com>
2024-08-20 22:12:37 -0300

    fix: auth add: inicial index

--- auth_ss.py
+++ auth_ss.py
@@ -2,27 +2,25 @@ import singlestoredb as s2
 from dotenv import load_dotenv
 import os
 
-load_dotenv("config.env")
 
-user_ss = os.getenv("SINGLESTORE_USER")
-password_ss = os.getenv("SINGLESTORE_PASSWORD")
+def singlestore_auth():
 
+    load_dotenv("config.env")
 
-# Create a connection to the database
-conn = s2.connect(
-    user_ss
-    + ":"
-    + password_ss
-    + "@svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com:3333/db_yago_3770b"
-)
-# Check if the connection is open
-with conn:
-    with conn.cursor() as cur:
-        flag = cur.is_connected()
-        print(flag)
+    user_ss = os.getenv("SINGLESTORE_USER")
+    password_ss = os.getenv("SINGLESTORE_PASSWORD")
+
+    # Create a connection to the database
+    conn = s2.connect(
+        user_ss
+        + ":"
+        + password_ss
+        + "@svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com:3333/db_yago_3770b"
+    )
+    return conn
 
 
-with conn.cursor() as cur:
-    cur.execute("SELECT * FROM embeddings")
-    for row in cur.fetchall():
-        print(row)
+# conn = auth_ss()
+# with conn.cursor() as cur:
+#     sql = cur.execute("SELECT * FROM embeddings")
+#     print(sql)
--- index_ss.py
+++ index_ss.py
@@ -2,5 +2,13 @@ import requests
 import os
 import json
 from dotenv import load_dotenv
+from auth_ss import singlestore_auth
 
 load_dotenv("config.env")
+
+
+conn = singlestore_auth()
+
+with conn.cursor() as cur:
+    sql = cur.execute("SELECT * FROM embeddings")
+    print(sql)
